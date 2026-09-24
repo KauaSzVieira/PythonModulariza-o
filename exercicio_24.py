7bd9cef6c23ca47ec50507d14b705fd9e957f2ad
KauaSzVieira <kaua071220@gmail.com>
2026-09-24 17:23:38 -0300

    Update exercicio_24.py

--- exercicio_24.py
+++ exercicio_24.py
@@ -7,6 +7,10 @@ def verificar():
 
     if numero % 2 == 0 and numero % 3 == 0:
         print("É divisível por 2 e 3")
+    elif numero % 2 == 0 and numero % 3 != 0:
+        print("É divisível por 2 e não é divisível por 3")
+    elif numero % 2 != 0 and numero % 3 == 0:
+        print("Não é divisível por 2 e é divisível por 3")
     else:
         print("Não é divisível por 2 e 3")
 
